ac5ac211b5809b2f6b7a37adec11704ea1f312fe
Sarzzzo <gamerliashd0610@gmail.com>
2026-01-24 20:08:33 -0600

    WORKSHOP1,  One slight additional feature is the creation of the ".gitignore" file to prevent the more than 1771 added files from appearing in the commits made by Visual Studio.

---
+++ Workshop1/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const routes = require('./routes/routes');
+
+const app = express();
+app.use(express.json());
+
+// Rutas (prefijo /api)
+app.use('/api', routes);
+
+// Conexión Mongo
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(' Error MongoDB:', error);
+});
+
+database.once('connected', () => {
+  console.log(' Database Connected');
+});
+
+// Levantar server
+app.listen(3000, () => {
+  console.log(` Server Started at 3000`);
+});
